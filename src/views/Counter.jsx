9e74723c1729201813d737424dc30ca64dfd1669
Sleepyheadp <sleepyheadp322@gmail.com>
2024-07-25 16:47:53 +0800

    update

--- src/views/Counter.jsx
+++ src/views/Counter.jsx
@@ -6,11 +6,12 @@ const useStore = create((set) => ({
 	count: 0,
 	increase: () => set((state) => ({ count: state.count + 1 })),
 	decrease: () => set((state) => ({ count: state.count - 1 })),
+	removeAll: () => set({ count: 0 }),
 }));
 
 // 定义 Counter 组件
 function Counter() {
-	const { count, decrease } = useStore();
+	const { count, decrease, removeAll } = useStore();
 	// 也可以这样使用
 	const increase = useStore((state) => state.increase);
 	return (
@@ -19,6 +20,7 @@ function Counter() {
 			<h2>{count}</h2>
 			<button onClick={increase}>Increase</button>
 			<button onClick={decrease}>Decrease</button>
+			<button onClick={removeAll}>remove</button>
 		</div>
 	);
 }
